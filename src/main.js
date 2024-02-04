21f4ab32ef2618bcb970432c1609b45fd3a10250
Veronika <veronikapetrushka@gmail.com>
2024-02-04 15:55:03 +0100

    modal issues

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import Swal from 'sweetalert2';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import './css/loading.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const searchForm = document.querySelector('.form');
   const searchBar = document.getElementById('search-bar');
   const submitBtn = document.querySelector('.submit-btn');
   const gallery = document.getElementById('gallery');
+  const loading = document.querySelector('.loading');
 
   searchForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -17,10 +20,11 @@ document.addEventListener('DOMContentLoaded', () => {
       iziToast.error({
         title: 'Error',
         message: 'Please enter a search query.',
-        position: 'topRight',
       });
       return;
     }
+
+    loading.style.display = 'block';
 
     const apiKey = '42199698-86ec1f577e997e517f326ea6b';
     const url = `https://pixabay.com/api/?key=${apiKey}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`;
@@ -28,31 +32,46 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        if (data.results.length === 0) {
+        loading.style.display = 'none';
+        if (data.hits.length === 0) {
           iziToast.error({
             title: 'Error',
             message:
               'Sorry, there are no images matching your search query. Please try again!',
-            position: 'topRight',
           });
           gallery.innerHTML = '';
         } else {
           gallery.innerHTML = '';
-          data.results.forEach(image => {
+          data.hits.forEach(image => {
             const card = document.createElement('div');
             card.className = 'card';
             card.innerHTML = `
-    <img src="${image.webformatURL}" alt="${image.tags}" onclick="showModal('${image.largeImageURL}')" />
+    <a href="${image.largeImageURL}" data-lightbox="gallery">
+      <img src="${image.webformatURL}" alt="${image.tags}"/>
+    </a>
     <div class="card-info">
-      <h3>${image.tags}</h3>
-      <p>Likes: ${image.likes}</p>
-      <p>Views: ${image.views}</p>
-      <p>Comments: ${image.comments}</p>
-      <p>Downloads: ${image.downloads}</p>
-    </div>
-  `;
+      <h3 class="img-desc">${image.tags}</h3>
+      <div class="img-info">
+      <p class="img-stat">Likes:<br>${image.likes}</p>
+      <p class="img-stat">Views:<br>${image.views}</p>
+      <p class="img-stat">Comments:<br>${image.comments}</p>
+      <p class="img-stat">Downloads:<br>${image.downloads}</p>
+      </div>
+              </div>
+   `;
             gallery.appendChild(card);
           });
+
+          const lightbox = new SimpleLightbox('.gallery a', {
+            captions: true,
+            captionsData: 'alt',
+            captionDelay: 250,
+            captionPosition: 'bottom',
+            history: false,
+            animationSpeed: 250,
+            close: true,
+          });
+          lightbox.refresh();
         }
       })
       .catch(error => {
@@ -60,11 +79,3 @@ document.addEventListener('DOMContentLoaded', () => {
       });
   });
 });
-
-function showModal(imageUrl) {
-  Swal.fire({
-    title: 'Image',
-    imageUrl: imageUrl,
-    imageAlt: 'Large image',
-  });
-}
